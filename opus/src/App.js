5580f780efc0fb528e4ce9c66e4af123b7e6fb67
Rohit Malviya <rohitbmalviya@gmail.com>
2024-01-15 19:43:17 +0530

    Created different pages

--- opus/src/App.js
+++ opus/src/App.js
@@ -1,5 +1,26 @@
+import { Box } from "@mui/material";
+import Navbar from "./Componets/Navbar";
+import Footer from "./Componets/Footer";
+import Section1 from "./Componets/Sections/Section1.js";
+import Section2 from "./Componets/Sections/Section2.js";
+import Section3 from "./Componets/Sections/Section3.js";
+import Section4 from "./Componets/Sections/Section3.js";
+import Section5 from "./Componets/Sections/Section3.js";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Box>
+        <Navbar />
+        <Section1 />
+        <Section2 />
+        <Section3 />
+        <Section4 />
+        <Section5 />
+        <Footer />
+      </Box>
+    </>
+  );
 }
 
 export default App;
---
+++ opus/src/Componets/Footer.js
@@ -0,0 +1,29 @@
+import { Box, Grid, Stack } from "@mui/material";
+import React from "react";
+
+function Footer() {
+  const overview = [
+    "Overview",
+    "Landing",
+    "Company",
+    "Pricing",
+    "CMS Pages",
+    "Accounts",
+    "Buy for Figma",
+  ];
+  const Pages = ["Langing "];
+  return (
+    <>
+      <Box>
+        <Stack>
+          <Grid></Grid>
+        </Stack>
+        <Stack></Stack>
+        <Stack></Stack>
+        <Stack></Stack>
+      </Box>
+    </>
+  );
+}
+
+export default Footer;
